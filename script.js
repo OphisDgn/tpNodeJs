aaeb46cc72823f87174ca06293751417a5e68421
Celine Lieutaud <celine.lieutaud@ynov.com>
2021-03-08 17:30:39 +0100

    Essaie ajout cookie
    Essaie de créer des cookies pour la persistance des données

--- script.js
+++ script.js
@@ -6,6 +6,40 @@ app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`)
 })
 
+/* FONCTION POUR L'UTLISATION DES COOKIES */
+function setCookie(cname, cvalue) {
+    var d = new Date();
+    d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie(msg) {
+    var cook = getCookie("datas");
+    if (cook != "") {
+      io.emit('chatMsg', msg);
+    } else {
+      if (msg != "" && msg != null) {
+        setCookie("datas", msg);
+      }
+    }
+}
+
+
 io.on('connection', (socket) => {
     console.log('Un utilisateur s\'est connecté !');
 
@@ -13,7 +47,8 @@ io.on('connection', (socket) => {
         console.log("Un utilisateur s'est déconnecté !");
     });
     socket.on('chatMsg', (msg) => {
-        io.emit('chatMsg', msg);
+        //io.emit('chatMsg', msg);
+        checkCookie(msg);
     });
 })
 
