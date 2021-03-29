003e1bdc1da813c1e64dfb6b7b9231813b2f5db5
Celine Lieutaud <celine.lieutaud@ynov.com>
2021-03-29 17:41:27 +0200

    essaie chat avec mongodb

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # tpNodeJs
 Repository pour le Travail Pratique (dit TP) du cours de NodeJs.
 
-## Consignes : 
+## Consignes branch Main : 
 Réaliser un chat en temps réel (sans persistances de données)
 Le chat fonctionne en localhost et utiliser deux onglets de navigation pour simuler deux utilisateur du chat
 lancer plein de requete pour vérifier que le code ne soit pas bloquant (synchrone)
 
 
+## Consignes branche "WithMongoDb" : 
+Réaliser un chat en temps réel avec persistance des données en utilisant une BDD MongoDb.
+Une collection qui stocke les users et les messages.
+
+Bonus: séparer les users et les messages dans deux collections.
+
+/!\ Je n'arrive pas à me connecter avec MongoCompass /!\
 
 
 > Année 2020 - 2021
--- script.js
+++ script.js
@@ -2,59 +2,47 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`)
-})
+const { MongoDb, MongoClient} = require('mongodb');
 
-/* FONCTION POUR L'UTLISATION DES COOKIES */
-/*
-function setCookie(cname, cvalue) {
-    var d = new Date();
-    d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
+// ! Je n'arrive pas à me connecter avec MongoCompass
+// Même en ne mettant que " mongodb://localhost:27017 " 
 
-function checkCookie(msg) {
-    var cook = getCookie("datas");
-    if (cook != "") {
-      io.emit('chatMsg', msg);
+const uri = 'mongodb://localhost:27017/MongoDb';
+const client = new MongoClient(uri);
+
+client.connect((err, instance) => {
+    if (err) {
+        process.exit(1);
     } else {
-      if (msg != "" && msg != null) {
-        setCookie("datas", msg);
-      }
+        app.get("/", (req, res) => res.redirect('/chat'))
+
+        const db = instance.db('MongoDb');
+        const collection = db.collection('MsgChats');
+        collection.insertOne({Pseudo: 'Steve', Message: 'Hello World'});
+
+        /*
+        
+        app.get("/", (req, res) => {
+            res.sendFile(`${__dirname}/public/index.html`)
+        })
+        io.on('connection', (socket) => {
+            console.log('Un utilisateur s\'est connecté !');
+        
+            socket.on('disconnect', () => {
+                console.log("Un utilisateur s'est déconnecté !");
+            });
+            socket.on('chatMsg', (msg) => {
+                io.emit('chatMsg', msg);
+            });
+        })*/
+        server.listen(3000, () => {
+            console.log('Ecoute sur le port 3000');
+        });
     }
-}
-*/
-
-io.on('connection', (socket) => {
-    console.log('Un utilisateur s\'est connecté !');
-
-    socket.on('disconnect', () => {
-        console.log("Un utilisateur s'est déconnecté !");
-    });
-    socket.on('chatMsg', (msg) => {
-        io.emit('chatMsg', msg);
-        //checkCookie(msg);
-    });
 })
 
 
 
-server.listen(3000, () => {
-    console.log('Ecoute sur le port 3000');
-});
+
+
+
